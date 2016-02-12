5c20fa4c5273c4939810c07df92dfd280c4996b3
Iman Tabrizian <tabrizian@outlook.com>
2016-02-12 13:25:00 +0330

    Finalize q1.

--- HW2/q1-n-equations/main.cpp
+++ HW2/q1-n-equations/main.cpp
@@ -1,24 +1,35 @@
 #include <iostream>
 #include "equation.h"
 #include "lib.h"
+#include "fstream"
 
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n; //Number of equations
-    double **arr_coefficients;
+    char file_name[100];
+    cout<<"Enter the path:";
+    cin>>file_name;
+    ifstream input(file_name);
 
+    if(!input.is_open()){
+        cout<<"File doesn't exist!!";
+        return 0;
+    }
+
+    int n;
+    input >> n; //Number of equations
+    double **arr_coefficients;
     arr_coefficients = new double *[n];
+    
     for (int i = 0; i < n; ++i) {
         arr_coefficients[i] = new double[n];
         for (int j = 0; j < n; ++j) {
-            cin >> arr_coefficients[i][j];
+            input >> arr_coefficients[i][j];
         }
     }
     double *arr_c = new double[n];
     for (int l = 0; l < n; ++l) {
-        cin >> arr_c[l];
+        input >> arr_c[l];
     }
 
     double det_coefficients = det(arr_coefficients, n);
