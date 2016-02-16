93ce3bd854db399f081f9b5b0ac93d8d922139b4
Iman Tabrizian <tabrizian@outlook.com>
2016-02-16 23:11:23 +0330

    Completes amar questions.

--- HW2/amar-q2/main.cpp
+++ HW2/amar-q2/main.cpp
@@ -9,11 +9,108 @@ double create_random() {
 
 int main() {
     int n;
-    cin>>n;
+    cin >> n;
     double *pieces = new double[n];
     for (int i = 0; i < n; ++i) {
-        cin>>pieces[i];
+        cin >> pieces[i];
     }
-    
+
+    double sum = 0;
+    for (int m = 0; m < n; ++m) {
+        sum += pieces[m] * (m + 1);
+    }
+
+    cout << "The expected value is: " << sum / n << endl;
+
+    for (int k = 1; k < n; ++k) {
+        pieces[k] += pieces[k - 1];
+    }
+
+
+    int *repeats = new int[n];
+    for (int l = 0; l < n; ++l) {
+        repeats[l] = 0;
+    }
+
+    for (int j = 0; j < 30; ++j) {
+        double rand = create_random();
+        for (int i = 0; i < n; ++i) {
+            if (rand < pieces[i]) {
+                repeats[i]++;
+                break;
+            }
+
+        }
+    }
+
+    sum = 0;
+    for (int i1 = 0; i1 < n; ++i1) {
+        sum += ((double)repeats[i1]) / 30 * (i1 + 1);
+    }
+
+    cout << "The expected value for n = 30 is: " << sum / n << endl;
+    for (int l = 0; l < n; ++l) {
+        repeats[l] = 0;
+    }
+
+    for (int j = 0; j < 300; ++j) {
+        double rand = create_random();
+        for (int i = 0; i < n; ++i) {
+            if (rand < pieces[i]) {
+                repeats[i]++;
+                break;
+            }
+
+        }
+    }
+
+    sum = 0;
+    for (int i1 = 0; i1 < n; ++i1) {
+        sum += ((double)repeats[i1]) / 300 * (i1 + 1);
+    }
+
+    cout << "The expected value for n = 300 is: " << sum / n << endl;
+    for (int l = 0; l < n; ++l) {
+        repeats[l] = 0;
+    }
+
+    for (int j = 0; j < 3000; ++j) {
+        double rand = create_random();
+        for (int i = 0; i < n; ++i) {
+            if (rand < pieces[i]) {
+                repeats[i]++;
+                break;
+            }
+
+        }
+    }
+
+    sum = 0;
+    for (int i1 = 0; i1 < n; ++i1) {
+        sum += ((double)repeats[i1]) / 3000 * (i1 + 1);
+    }
+
+    cout << "The expected value for n = 3000 is: " << sum / n << endl;
+    for (int l = 0; l < n; ++l) {
+        repeats[l] = 0;
+    }
+
+    for (int j = 0; j < 30000; ++j) {
+        double rand = create_random();
+        for (int i = 0; i < n; ++i) {
+            if (rand < pieces[i]) {
+                repeats[i]++;
+                break;
+            }
+
+        }
+    }
+
+    sum = 0;
+    for (int i1 = 0; i1 < n; ++i1) {
+        sum += ((double)repeats[i1]) / 30000 * (i1 + 1);
+    }
+
+    cout << "The expected value for n = 30000 is: " << sum / n << endl;
     return 0;
 }
--- HW2/q3-amar/main.cpp
+++ HW2/q3-amar/main.cpp
@@ -5,10 +5,10 @@
 using namespace std;
 
 int main() {
-    ifstream input("distributions/poisson.txt");
-    int n;
-    int num = 0;
-    int sum = 0;
+    ifstream input("distributions/normal.txt");
+    double n;
+    double num = 0;
+    double sum = 0;
     double avg;
     while (input >> n) {
         sum += n;
@@ -16,7 +16,7 @@ int main() {
     }
 
     avg = ((double) sum / num);
-    cout<<"poisson.txt: "<<endl;
+    cout<<"normal.txt: "<<endl;
     cout << "Average is: " << avg << endl;
     input.clear();
     input.seekg(0, ios_base::beg);
@@ -28,29 +28,32 @@ int main() {
 
     variance = variance / num;
     cout <<"Variance is: "<< variance << endl;
-    cout<<"Enheraf meyar: " << sqrt(variance) <<endl;
+    cout<<"Standard Deviation: " << sqrt(variance) <<endl;
     input.close();
-    input.open("distributions/normal.txt");
-    while (input >> n) {
+
+    cout << endl;
+    ifstream input2("distributions/poisson.txt");
+    num = 0;
+    sum = 0;
+    while (input2 >> n) {
         sum += n;
         num++;
     }
 
-    cout <<endl;
     avg = ((double) sum / num);
-    cout<<"sample.txt: "<<endl;
+    cout<<"poisson.txt: "<< endl;
     cout << "Average is: " << avg << endl;
-    input.clear();
-    input.seekg(0, ios_base::beg);
+    input2.clear();
+    input2.seekg(0, ios_base::beg);
 
     variance = 0;
-    while (input >> n) {
+    while (input2 >> n) {
         variance += (n - avg) * (n - avg);
     }
 
     variance = variance / num;
     cout <<"Variance is: "<< variance << endl;
-    cout<<"Enheraf meyar: " << sqrt(variance) <<endl;
+    cout<<"Standard Deviation: " << sqrt(variance) <<endl;
     input.close();
     return 0;
 }
