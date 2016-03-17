8a6d05e1b06fe562d23255b4b83b394c770fc720
Iman Tabrizian <tabrizian@outlook.com>
2016-03-17 18:20:45 +0330

    First test.

--- HW5/src/find_largest_seq.cpp
+++ HW5/src/find_largest_seq.cpp
@@ -8,16 +8,32 @@ int max(int num1, int num2){
     return max_num;
 }
 
-int largest_seq(int *arr, int begin, int end, int max_num){
-    int max_length = 1;
+int largest_seq(int *arr, int index, int &max_num){
+    if(index == 0)
+        return 1;
 
+    int max_length = 1;
     int i = 0;
-    for(i = 0;i < end; i++){
-        int length = max(
+    int current_max = 1;
+    int max_num_in_seq;
+
+    for(i = 0;i < index; i++){
+        int number = largest_seq(arr, i, max_num_in_seq);
+        if(arr[index+1]>= max_num_in_seq){
+            number++;
+            max_num_in_seq = arr[index+1];
+        }
+        current_max = max(number, current_max);
     }
+
+    max_num = max_num_in_seq;
+    return current_max;
 }
 
 int main(){
+    int arr[]={3, 4, -1, 0, 6, 2, 3};
+    int max_num;
+    cout<<largest_seq(arr, 5, max_num)<<endl;
 
     return 0;
 }
