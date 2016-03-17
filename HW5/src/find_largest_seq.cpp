14de4f00eb380bfcd3190252950bf0c985fc0ad9
Iman Tabrizian <tabrizian@outlook.com>
2016-03-17 14:41:13 +0330

    Adds CMake

---
+++ HW5/src/find_largest_seq.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int max(int num1, int num2){
+    int max_num = num1;
+    if(num2 > num1)
+        max_num = num2;
+    return max_num;
+}
+
+int largest_seq(int *arr, int begin, int end, int max_num){
+    int max_length = 1;
+
+    int i = 0;
+    for(i = 0;i < end; i++){
+        int length = max(
+    }
+}
+
+int main(){
+
+    return 0;
+}
