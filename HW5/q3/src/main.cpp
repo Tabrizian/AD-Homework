be12303bf4101c6a5757346a9f240ce25999cec3
Iman Tabrizian <tabrizian@outlook.com>
2016-03-20 00:03:39 +0330

    Changes algorithm because of misunderstanding in problem :(

--- HW5/q3/src/main.cpp
+++ HW5/q3/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 struct data{
@@ -14,11 +15,14 @@ int largest_seq_dp(struct data *arr, int size){
 
     for(int i = 0; i < size; i++){
         for(int j = 0; j < i; j++){
-            if(arr[i].width >= arr[j].width && arr[i].heigth >= arr[j].heigth){
+            if(arr[i].heigth <= arr[j].heigth){
                 int temp = calculated_data[j] + 1;
                 if(temp > calculated_data[i]){
                     calculated_data[i] = temp;
                 }
+            } else {
+                if(calculated_data[j] > calculated_data[i])
+                    calculated_data[i] = calculated_data[j];
             }
         }
     }
@@ -33,6 +37,22 @@ void get_input(struct data *arr, int size){
     }
 }
 
+int compare_data(const void *i1, const void *i2){
+    struct data a = *((struct data *) i1);
+    struct data b = *((struct data *) i2);
+
+    if(a.width > b.width)
+        return 1;
+    else if(a.width < b.width)
+        return -1;
+    else if(a.heigth > b.heigth)
+        return 1;
+    else if(a.heigth < b.heigth)
+        return -1;
+    else
+        return 0;
+}
+
 int main(){
 
     int n;
@@ -40,8 +60,9 @@ int main(){
     cin>>n;
     struct data *arr = new struct data[n];
     get_input(arr,n);
+    qsort(arr, n, sizeof(struct data), compare_data);
 
-    cout<<n - largest_seq_dp(arr, n)<<endl;
+    cout<<largest_seq_dp(arr, n)<<endl;
 
     return 0;
 }
